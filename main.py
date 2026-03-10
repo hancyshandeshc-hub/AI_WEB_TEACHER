c769f0031e14745048a264b3d846b181a746f572
hancyshandeshc-hub <hancyshandeshc@gmail.com>
2026-03-10 17:23:33 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from langchain_core.messages import SystemMessage,HumanMessage,AIMessage
 from langchain_core.prompts import ChatPromptTemplate
-import os
-os.environ["HUGGINGFACEHUB_API_TOKEN"]="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"
+from dotenv import load_dotenv
+load_dotenv()
 llm=HuggingFaceEndpoint(
     repo_id="Qwen/Qwen2.5-Coder-7B-Instruct",
     task="text-generation"
@@ -24,6 +24,7 @@ result=model.invoke(prompt)
 button=st.button("Result")
 if button or prompt:
     st.write(result.content)
+
 
 
 
